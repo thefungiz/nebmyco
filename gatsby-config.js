133683717f6186f1229d775a2e95eee8c38b943a
Derek Zeller <root.dzeller@gmail.com>
2020-12-30 11:23:31 -0600

    updated title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Nebraska Mycological Society',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Community of fungi enthusiasts in the Cornhusker state.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
