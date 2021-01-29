0b94793c56f59e74dfafc0b078461ebfdffe009e
Derek Zeller <root.dzeller@gmail.com>
2021-01-28 18:06:29 -0600

    add footer logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
+import logo from '../img/NMS-inv-trans.png'
 import facebook from '../img/social/facebook.svg'
 
 const Footer = class extends React.Component {
@@ -11,8 +11,8 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
+            alt="Logo"
+            style={{ width: '14em', height: '14em' }}
           />
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
