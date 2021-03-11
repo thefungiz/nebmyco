44094635044f629cc74536e525f22c108414587d
Derek Zeller <root.dzeller@gmail.com>
2021-03-10 21:42:23 -0600

    added list view for calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,10 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Calendar = ({ }) => {
-    const calendarUrl = 'https://calendar.google.com/calendar/embed?src=' + encodeURI('nebmyco@gmail.com')
-    return (<div className="calendarWrapper">
-        <iframe className="calendar" src={calendarUrl} width="1200" height="600" frameborder="0" scrolling="no"></iframe>
-    </div>)
+    const baseCalendarUrl = `https://calendar.google.com/calendar/embed?${encodeURI('src=nebmyco@gmail.com&ctz=America/Chicago')}`
+    const agendaCalendarUrl = `${baseCalendarUrl}${encodeURI('&mode=AGENDA&showTabs=0&showTitle=0&showNav=0&showDate=0')}`
+    return (
+        <>
+            <div className="calendarWrapper">
+                <iframe src={baseCalendarUrl}></iframe>
+            </div>
+            <div className="calendarWrapper">
+                <iframe src={agendaCalendarUrl}></iframe>
+            </div>
+        </>
+    )
 }
 
 Calendar.propTypes = {
