aacd6e6868a0318c78b8ae50a2620406ddc8de38
Derek Zeller <root.dzeller@gmail.com>
2021-03-10 20:56:32 -0600

    add simple responsiveness to calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { v4 } from 'uuid'
 
 const Calendar = ({ }) => {
     const calendarUrl = 'https://calendar.google.com/calendar/embed?src=' + encodeURI('nebmyco@gmail.com')
