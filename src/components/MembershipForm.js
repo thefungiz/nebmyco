a43f745d7926e071cfc7a6c47f977b081dbde57e
Derek Zeller <root.dzeller@gmail.com>
2021-10-21 17:51:01 -0500

    functional email sender

--- src/components/MembershipForm.js
+++ src/components/MembershipForm.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react'
+import { init, sendForm } from 'emailjs-com';
+import emailkey from '../emailkey';
+
+init(emailkey.USER_ID);
 
 const MembershipForm = () => {
+
+  const [isFormCompleted, setIsFormCompleted] = useState(false);
 
   const [firstName, setFirstName] = useState(undefined);
   const [lastName, setLastName] = useState(undefined);
@@ -29,27 +35,36 @@ const MembershipForm = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log('submitted!');
+    sendForm(emailkey.SERVICE_ID, emailkey.TEMPLATE_ID, e.target)
+      .then(() => console.log('success'))
+      .catch(e => { console.error('there was an error'); console.error(e)});
+    setIsFormCompleted(true);
   };
 
-  const regionList = ['Omaha', 'Lincoln', 'Tri-Cities', 'Other'];
+  const regionList = ['Omaha', 'Lincoln', 'Tri-Cities', 'Other (Specify in the "Message" field)'];
 
-  return (
-    <div className="box">
-      <h2 className="title">Step 1 - Your Information</h2>
+  const boxContent = isFormCompleted ?
+    (
+      <div>
+        <p className="subtitle">Complete!</p>
+        <p>Thanks for filling out the signup form! You should receive an email from us shortly.</p>
+      </div>
+    )
+    : (
+      <div>
       <p className="subtitle">All fields are required unless marked optional.</p>
       <form action="mailto:nebmyco@gmail.com" encType="multipart/form-data" method="post" onSubmit={handleSubmit}>
         <div className="field">
           <label className="label">
             First Name
           </label>
-          <input className="input is-rounded" type="text" name="first.name" value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="Mycena" />
+          <input className="input is-rounded" type="text" name="firstName" value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="Mycena" />
         </div>
         <div className="field">
           <label className="label">
             Last Name
           </label>
-          <input className="input is-rounded" type="text" name="last.name" value={lastName} onChange={e => setLastName(e.target.value)} placeholder="McMycelium" />
+          <input className="input is-rounded" type="text" name="lastName" value={lastName} onChange={e => setLastName(e.target.value)} placeholder="McMycelium" />
         </div>
         <div className="field">
           <label className="label">
@@ -57,38 +72,38 @@ const MembershipForm = () => {
           </label>
           <input className="input is-rounded has-icons-left" type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="ganoderma@gmail.com" />
           {/* <span class="icon is-small is-left">
-            <i class="fas fa-envelope"></i>
-          </span> */}
+          <i class="fas fa-envelope"></i>
+        </span> */}
         </div>
         <div className="field">
           <label className="label">
             Address Line 1
           </label>
-          <input className="input is-rounded" type="text" name="address.line1" value={addressLine1} onChange={e => setAddressLine1(e.target.value)} placeholder="445 Mycological Way" />
+          <input className="input is-rounded" type="text" name="addressLine1" value={addressLine1} onChange={e => setAddressLine1(e.target.value)} placeholder="445 Mycological Way" />
         </div>
         <div className="field">
           <label className="label">
             Address Line 2
           </label>
-          <input className="input is-rounded" type="text" name="address.line2" value={addressLine2} onChange={e => setAddressLine2(e.target.value)} placeholder="Apartment 2" />
+          <input className="input is-rounded" type="text" name="addressLine2" value={addressLine2} onChange={e => setAddressLine2(e.target.value)} placeholder="Apartment 2" />
         </div>
         <div className="field">
           <label className="label">
             City
           </label>
-          <input className="input is-rounded" type="text" name="address.city" value={addressCity} onChange={e => setAddressCity(e.target.value)} placeholder="Mushroom City" />
+          <input className="input is-rounded" type="text" name="addressCity" value={addressCity} onChange={e => setAddressCity(e.target.value)} placeholder="Mushroom City" />
         </div>
         <div className="field">
           <label className="label">
             State
           </label>
-          <input className="input is-rounded" type="text" name="address.state" value={addressState} onChange={e => setAddressState(e.target.value)} placeholder="Nebraska" />
+          <input className="input is-rounded" type="text" name="addressState" value={addressState} onChange={e => setAddressState(e.target.value)} placeholder="Nebraska" />
         </div>
         <div className="field">
           <label className="label">
             Zip
           </label>
-          <input className="input is-rounded" type="text" name="address.zip" value={addressZip} onChange={e => setAddressZip(e.target.value)} placeholder="77777" />
+          <input className="input is-rounded" type="text" name="addressZip" value={addressZip} onChange={e => setAddressZip(e.target.value)} placeholder="77777" />
         </div>
         <div className="field">
           <label className="label">
@@ -98,7 +113,7 @@ const MembershipForm = () => {
             {regionList.map(x => {
               return (
                 <label key={x} className="radio">
-                  <input type="radio" id={x.toLowerCase()} name="region" style={{marginRight: '.5em'}} />
+                  <input type="radio" id={x.toLowerCase()} value={x} name="region" style={{ marginRight: '.5em' }} />
                   {x}
                 </label>)
             })}
@@ -109,14 +124,14 @@ const MembershipForm = () => {
             Message (Optional)
           </label>
           <div className="control">
-          <textarea className="textarea is-rounded" rows="10" name="message" placeholder="Tell us a little bit about you." />
+            <textarea className="textarea is-rounded" rows="10" name="message" placeholder="Tell us a little bit about you." />
           </div>
         </div>
         <div className="field">
-        <label className="checkbox">
-          <input style={{marginRight: '.5em'}} type="checkbox" checked={liabilityAgreement} onChange={() => setLiabilityAgreement(!liabilityAgreement)} name="waiver.agreement" />
-          I agree to the <a style={{color: 'blue', fontWeight: 'bold'}} target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1WVk9yvwaLb6NK72x21cOeH0LThThbDAm/view?usp=sharing">Liability Waiver</a>
-        </label>
+          <label className="checkbox">
+            <input style={{ marginRight: '.5em' }} type="checkbox" checked={liabilityAgreement} onChange={() => setLiabilityAgreement(!liabilityAgreement)} name="waiverAgreement" />
+            I agree to the <a style={{ color: 'blue', fontWeight: 'bold' }} target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1WVk9yvwaLb6NK72x21cOeH0LThThbDAm/view?usp=sharing">Liability Waiver</a>
+          </label>
         </div>
         <div className="field">
           <div className="control">
@@ -124,6 +139,13 @@ const MembershipForm = () => {
           </div>
         </div>
       </form>
+      </div>
+    )
+
+  return (
+    <div className="box">
+      <h2 className="title">Step 1 - Your Information</h2>
+      {boxContent}
     </div>
   )
 };
