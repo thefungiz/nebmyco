a4f5a66623f97f918f8b8e84255a95d41bb5e798
Derek Zeller <root.dzeller@gmail.com>
2021-10-21 18:41:14 -0500

    use env and set up recaptcha

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 https://github.com/netlify-templates/gatsby-starter-netlify-cms
 
+Captcha stuff - https://github.com/imorente/gatsby-netlify-form-example
+
 # Gatsby + Netlify CMS Starter
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/b654c94e-08a6-4b79-b443-7837581b1d8d/deploy-status)](https://app.netlify.com/sites/gatsby-starter-netlify-cms-ci/deploys)
--- src/components/MembershipForm.js
+++ src/components/MembershipForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { init, sendForm } from 'emailjs-com';
-import emailkey from '../emailkey';
+import Recaptcha from "react-google-recaptcha";
 
-init(emailkey.USER_ID);
+const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
+
+init(process.env.USER_ID);
 
 const MembershipForm = () => {
 
@@ -18,6 +20,7 @@ const MembershipForm = () => {
   const [addressZip, setAddressZip] = useState(undefined);
   const [region, setRegion] = useState(undefined);
   const [liabilityAgreement, setLiabilityAgreement] = useState(false);
+  const [recaptchaVerified, setRecaptchaVerified] = useState(false);
 
   const requiredFields = [
     firstName,
@@ -29,16 +32,23 @@ const MembershipForm = () => {
     addressZip,
     region,
     liabilityAgreement,
+    recaptchaVerified,
   ]
 
   const isFormValid = requiredFields.every(x => x !== undefined && x !== '' && x !== false);
 
   const handleSubmit = e => {
     e.preventDefault();
-    sendForm(emailkey.SERVICE_ID, emailkey.TEMPLATE_ID, e.target)
-      .then(() => console.log('success'))
-      .catch(e => { console.error('there was an error'); console.error(e)});
+    sendForm(process.env.SERVICE_ID, process.env.TEMPLATE_ID, e.target)
+      .then(() => console.log('success')) // TODO present message to user
+      .catch(e => { console.error('there was an error'); console.error(e) });
     setIsFormCompleted(true);
+  };
+
+  const handleRecaptcha = value => {
+    if (value) {
+      setRecaptchaVerified(true);
+    }
   };
 
   const regionList = ['Omaha', 'Lincoln', 'Tri-Cities', 'Other (Specify in the "Message" field)'];
@@ -46,99 +56,106 @@ const MembershipForm = () => {
   const boxContent = isFormCompleted ?
     (
       <div>
-        <p className="subtitle">Complete!</p>
+        <p className="subtitle">Complete.</p>
         <p>Thanks for filling out the signup form! You should receive an email from us shortly.</p>
       </div>
     )
     : (
       <div>
-      <p className="subtitle">All fields are required unless marked optional.</p>
-      <form action="mailto:nebmyco@gmail.com" encType="multipart/form-data" method="post" onSubmit={handleSubmit}>
-        <div className="field">
-          <label className="label">
-            First Name
-          </label>
-          <input className="input is-rounded" type="text" name="firstName" value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="Mycena" />
-        </div>
-        <div className="field">
-          <label className="label">
-            Last Name
-          </label>
-          <input className="input is-rounded" type="text" name="lastName" value={lastName} onChange={e => setLastName(e.target.value)} placeholder="McMycelium" />
-        </div>
-        <div className="field">
-          <label className="label">
-            Email
-          </label>
-          <input className="input is-rounded has-icons-left" type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="ganoderma@gmail.com" />
-          {/* <span class="icon is-small is-left">
+        <p className="subtitle">All fields are required unless marked optional.</p>
+        <form data-netlify="true" data-netlify-recaptcha="true" encType="multipart/form-data" method="post" onSubmit={handleSubmit}>
+          <div className="field">
+            <label className="label">
+              First Name
+            </label>
+            <input className="input is-rounded" type="text" name="firstName" value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="Mycena" />
+          </div>
+          <div className="field">
+            <label className="label">
+              Last Name
+            </label>
+            <input className="input is-rounded" type="text" name="lastName" value={lastName} onChange={e => setLastName(e.target.value)} placeholder="McMycelium" />
+          </div>
+          <div className="field">
+            <label className="label">
+              Email
+            </label>
+            <input className="input is-rounded has-icons-left" type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="ganoderma@gmail.com" />
+            {/* <span class="icon is-small is-left">
           <i class="fas fa-envelope"></i>
         </span> */}
-        </div>
-        <div className="field">
-          <label className="label">
-            Address Line 1
-          </label>
-          <input className="input is-rounded" type="text" name="addressLine1" value={addressLine1} onChange={e => setAddressLine1(e.target.value)} placeholder="445 Mycological Way" />
-        </div>
-        <div className="field">
-          <label className="label">
-            Address Line 2
-          </label>
-          <input className="input is-rounded" type="text" name="addressLine2" value={addressLine2} onChange={e => setAddressLine2(e.target.value)} placeholder="Apartment 2" />
-        </div>
-        <div className="field">
-          <label className="label">
-            City
-          </label>
-          <input className="input is-rounded" type="text" name="addressCity" value={addressCity} onChange={e => setAddressCity(e.target.value)} placeholder="Mushroom City" />
-        </div>
-        <div className="field">
-          <label className="label">
-            State
-          </label>
-          <input className="input is-rounded" type="text" name="addressState" value={addressState} onChange={e => setAddressState(e.target.value)} placeholder="Nebraska" />
-        </div>
-        <div className="field">
-          <label className="label">
-            Zip
-          </label>
-          <input className="input is-rounded" type="text" name="addressZip" value={addressZip} onChange={e => setAddressZip(e.target.value)} placeholder="77777" />
-        </div>
-        <div className="field">
-          <label className="label">
-            Region
-          </label>
-          <fieldset className="control" value={region} onChange={e => setRegion(e.target.value)}>
-            {regionList.map(x => {
-              return (
-                <label key={x} className="radio">
-                  <input type="radio" id={x.toLowerCase()} value={x} name="region" style={{ marginRight: '.5em' }} />
-                  {x}
-                </label>)
-            })}
-          </fieldset>
-        </div>
-        <div className="field">
-          <label className="label">
-            Message (Optional)
-          </label>
-          <div className="control">
-            <textarea className="textarea is-rounded" rows="10" name="message" placeholder="Tell us a little bit about you." />
           </div>
-        </div>
-        <div className="field">
-          <label className="checkbox">
-            <input style={{ marginRight: '.5em' }} type="checkbox" checked={liabilityAgreement} onChange={() => setLiabilityAgreement(!liabilityAgreement)} name="waiverAgreement" />
-            I agree to the <a style={{ color: 'blue', fontWeight: 'bold' }} target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1WVk9yvwaLb6NK72x21cOeH0LThThbDAm/view?usp=sharing">Liability Waiver</a>
-          </label>
-        </div>
-        <div className="field">
-          <div className="control">
-            <button disabled={!isFormValid} className="button is-link" type="submit">Submit</button>
+          <div className="field">
+            <label className="label">
+              Address Line 1
+            </label>
+            <input className="input is-rounded" type="text" name="addressLine1" value={addressLine1} onChange={e => setAddressLine1(e.target.value)} placeholder="445 Mycological Way" />
           </div>
-        </div>
-      </form>
+          <div className="field">
+            <label className="label">
+              Address Line 2
+            </label>
+            <input className="input is-rounded" type="text" name="addressLine2" value={addressLine2} onChange={e => setAddressLine2(e.target.value)} placeholder="Apartment 2" />
+          </div>
+          <div className="field">
+            <label className="label">
+              City
+            </label>
+            <input className="input is-rounded" type="text" name="addressCity" value={addressCity} onChange={e => setAddressCity(e.target.value)} placeholder="Mushroom City" />
+          </div>
+          <div className="field">
+            <label className="label">
+              State
+            </label>
+            <input className="input is-rounded" type="text" name="addressState" value={addressState} onChange={e => setAddressState(e.target.value)} placeholder="Nebraska" />
+          </div>
+          <div className="field">
+            <label className="label">
+              Zip
+            </label>
+            <input className="input is-rounded" type="text" name="addressZip" value={addressZip} onChange={e => setAddressZip(e.target.value)} placeholder="77777" />
+          </div>
+          <div className="field">
+            <label className="label">
+              Region
+            </label>
+            <fieldset className="control" value={region} onChange={e => setRegion(e.target.value)}>
+              {regionList.map(x => {
+                return (
+                  <label key={x} className="radio">
+                    <input type="radio" id={x.toLowerCase()} value={x} name="region" style={{ marginRight: '.5em' }} />
+                    {x}
+                  </label>)
+              })}
+            </fieldset>
+          </div>
+          <div className="field">
+            <label className="label">
+              Message (Optional)
+            </label>
+            <div className="control">
+              <textarea className="textarea is-rounded" rows="10" name="message" placeholder="Tell us a little bit about you." />
+            </div>
+          </div>
+          <div className="field">
+            <label className="checkbox">
+              <input style={{ marginRight: '.5em' }} type="checkbox" checked={liabilityAgreement} onChange={() => setLiabilityAgreement(!liabilityAgreement)} name="waiverAgreement" />
+              I agree to the <a style={{ color: 'blue', fontWeight: 'bold' }} target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1WVk9yvwaLb6NK72x21cOeH0LThThbDAm/view?usp=sharing">Liability Waiver</a>
+            </label>
+          </div>
+          <div className="field">
+            <Recaptcha
+              // ref="recaptcha"
+              sitekey={RECAPTCHA_KEY}
+              onChange={handleRecaptcha}
+            />
+          </div>
+          <div className="field">
+            <div className="control">
+              <button disabled={!isFormValid} className="button is-link" type="submit">Submit</button>
+            </div>
+          </div>
+        </form>
       </div>
     )
 
