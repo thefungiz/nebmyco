ea03b847781eafd8040844bbffc1e5142cf36b94
thefungiz <8988383+thefungiz@users.noreply.github.com>
2022-02-02 21:48:24 -0600

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -27,18 +27,6 @@ export const BlogPostTemplate = ({
             </h1>
             <p>{description}</p>
             <div className="blog-post-content"><PostContent content={content} /></div>
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map((tag) => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
           </div>
         </div>
       </div>
