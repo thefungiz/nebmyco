014e58cd63b6eb3c72b4a81c84256ee77abc0a44
thefungiz <8988383+thefungiz@users.noreply.github.com>
2022-02-09 08:41:35 -0600

    Update MembershipDueTile.js

--- src/components/MembershipDueTile.js
+++ src/components/MembershipDueTile.js
@@ -6,7 +6,7 @@ const MembershipDueTile = () => {
     <div className="box">
       <h2 className="title">Step 2 - Pay Dues</h2>
       <p>We offer a $5 discount if our members pay for 2 years in advance as well as a family discount (price includes all members in a household).</p>
-      <p>NOTE: Please ensure that the prior form, "Step 1", has been compeleted prior to payment so that we can process your membership correctly. Thanks!</p>
+      <p>NOTE: Please ensure that the previous form, "Step 1", has been compeleted prior to payment so that we can process your membership correctly. Thanks!</p>
       <div className="columns">
             <div className="column">
                 Type and Duration
