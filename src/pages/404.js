3c1593e00225b24406626b93078909432a83e66e
Derek Zeller <root.dzeller@gmail.com>
2022-05-17 19:46:35 -0500

    name 2

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 const NotFoundPage = () => (
   <Layout>
     <div>
-      <h1>Not Found</h1>
+      <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </div>
   </Layout>
