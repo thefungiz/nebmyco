52e0d13dd2ea8834c534e937783886ae469b1db8
Derek Zeller <root.dzeller@gmail.com>
2023-01-09 08:15:28 -0600

    hack to show calendar events

--- src/components/MycoCalendar.js
+++ src/components/MycoCalendar.js
@@ -5,13 +5,14 @@ const calendars = [{ calendarId: process.env.CALENDAR_ID }];
 const apiKey = process.env.GOOGLE_CAL_API_KEY;
 
 const MycoCalendar = () => {
+    const publicHolidays = 'src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t' // Hack to show events. GCal isn't showing the events without a click in the iframe for some reason without this larger calendar...
     const baseCalendarUrl = `https://calendar.google.com/calendar/embed?${encodeURI('src=nebmyco@gmail.com&ctz=America/Chicago')}`
-    const agendaCalendarUrl = `${baseCalendarUrl}${encodeURI('&mode=AGENDA&showTabs=0&showTitle=0&showNav=0&showDate=0')}`
+    const agendaCalendarUrl = `${baseCalendarUrl}${encodeURI(`&${publicHolidays}&mode=AGENDA&showTabs=0&showTitle=0&showNav=0&showDate=0`)}`
     return (
         <>
             <Calendar apiKey={apiKey} calendars={calendars} />
             <div className="calendarWrapper">
-                <iframe title="agenda.calendar" src={agendaCalendarUrl}></iframe>
+                <iframe title="agenda.calendar" scrolling="no" src={agendaCalendarUrl}></iframe>
             </div>
         </>
     )
