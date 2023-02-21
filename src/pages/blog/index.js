a9be247ce500abe006a2ee5c260ac28c0104d9f2
Derek Zeller <root.dzeller@gmail.com>
2023-02-20 22:27:43 -0600

    fix deploy

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
-import FullWidthImage from '../../components/FullWidthImage'
 
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <FullWidthImage img="/img/people/mushroom_circle.webp" title="" subheading="" />
         <div
           className="full-width-image-container margin-top-0"
           style={{
