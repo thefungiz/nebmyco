cf3e132f624e7dee2b683bbb835054030e8eab03
Derek Zeller <root.dzeller@gmail.com>
2023-05-21 13:08:56 -0500

    initial proto for givebutter

---
+++ src/components/Givebutter.js
@@ -0,0 +1,9 @@
+export const initializeGiveButter = (window) => {
+  window.Givebutter = window.Givebutter || function () {
+    (Givebutter.q = Givebutter.q || []).push(arguments);
+  };
+  Givebutter.l = +new Date;
+  window.Givebutter('setOptions', {
+    "accountId": "zk6TslqvYY4LNq36"
+  });
+}
---
+++ src/pages/donate/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import Layout from '../../components/Layout'
+
+export default class DonateIndex extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Helmet>
+          <title>Nebraska Mycological Society | Donate</title>
+          <meta name="description" content="" />
+          <meta property="og:title" content="Donate to the Nebraska Mycological Society" />
+          <meta property="og:description" content="" />
+        </Helmet>
+        <section className="section section--gradient">
+          <div className="container">
+            <div givebutter-element-id="LYWo3p"></div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
+}
