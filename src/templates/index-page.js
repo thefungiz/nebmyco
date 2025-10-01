bfe128b4d0d470a067e7ff777b2e619ce894fba1
thefungiz <8988383+thefungiz@users.noreply.github.com>
2025-10-01 11:08:38 -0500

    Update index-page.js
    Add Nebraska mushrooms website link to homepage

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -43,7 +43,14 @@ const IndexPageTemplate = ({
                       <p>{description}</p>
                     </div>
                   </div>
-                  <p>👉 Submit a mushroom identification request <a class="myco-link" target="_blank" rel="noreferrer" href="https://forms.gle/Lz6f6n63Pn4S3YBM9">here</a></p>
+                  <div className="column is-12">
+                    <h3 className="has-text-weight-semibold is-size-2">
+                      Nebraska Mushrooms Website 
+                    </h3>
+                    <div className="column is-12">
+                      Clic#k <Link to="https://www.nebraskamushrooms.org">here</Link> to check out our citizen science driven website containing mushroom profiles from 250+ known species surveyed from the state of Nebraska.
+                    </div>
+                  </div>                                   
                   <Features gridItems={intro.blurbs} />
                   <div className="column is-12">
                     <h3 className="has-text-weight-semibold is-size-2">
